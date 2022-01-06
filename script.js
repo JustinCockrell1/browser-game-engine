fec7f0c968bba44a56cceff64c2277916268a0d0
JustinCockrell1 <justin.cockrell1@gmail.com>
2022-01-06 04:18:05 -0600

    start testing of div positioning

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const body = document.getElementById("body");
+const body = document.querySelector(".mainscreen");
+
+
 
 const div = document.createElement("div");
 div.style.left = "10px";
 div.style.top = "100px";
+div.classList.add("object");
+let divX = 10;
 
 body.appendChild(div)
+
+window.setInterval(()=>{
+divX+=1;
+div.style.left = `${divX}px`;
+console.log("hello");
+
+}, 100);
