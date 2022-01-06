4e845d24ec1395a9578801f3bf65ff63b3b39e5f
JustinCockrell1 <justin.cockrell1@gmail.com>
2022-01-06 04:26:18 -0600

    add js folder and work on styles

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const body = document.querySelector(".mainscreen");
 
 
 const div = document.createElement("div");
+const btn = document.createElement("button");
 div.style.left = "10px";
 div.style.top = "100px";
 div.classList.add("object");
+btn.textContent="hello";
+div.appendChild(btn);
 let divX = 10;
 
 body.appendChild(div)
